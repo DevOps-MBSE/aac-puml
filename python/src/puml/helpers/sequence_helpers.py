9b8c126710e6fbfd0b0e5f70acb45d3f4bafe649
Lizz Condrey <ashley.condrey@ngc.com>
2024-04-11 17:45:24 +0000

    - got helper method for participant properly registered in generator - got helper method for participant properly incorporated to template - got helper method for participant properly implemented in sequence_helpers -WIP: still struggling with figuring out how to pass in the additional model types as data source via generator for template to use  - commit before prepping for meeting

--- python/src/puml/helpers/sequence_helpers.py
+++ python/src/puml/helpers/sequence_helpers.py
@@ -5,58 +5,45 @@ sequence diagrams in a PUML format.
 from typing import Any
 
 
-def get_use_case_participants(use_case: Any, use_case_actors: dict) -> list[dict]:
+def get_use_case_participant(use_case: Any, use_case_actor: dict) -> str:
     """
-    Helper method for extracting the participants from a use case definition.
+    Helper method for extracting the matching participant information from a use case definition.
 
     Args:
         use_case (Any): The use case definition from which to extract participants.
-        use_case_actors (dict): The dictionary of actors within the use case definition.
+        use_case_actor (dict): The actor definition for a use case participant to match against.
 
     Returns:
-        The list of participants and their data within the use case definition.
+        The participant actor model information.
     """
-    participants: list[dict] = []
-
     # declare participants
     use_case_participants = use_case["participants"]
     for use_case_participant in use_case_participants:  # each participant is a field type
-        if use_case_participant in use_case_actors.keys():
-            participant = use_case_actors[use_case_participant].structure["actor"]
+        if use_case_participant in use_case_actor.keys():
+            participant = use_case_actor.structure["actor"]
             if "model" in participant.keys():
-                participants.append(
-                    {
-                        "type": participant["model"],
-                        "name": participant["name"],
-                    }
-                )
+                return f"{participant['type']} as {participant['name']}"
             else:
-                participants.append(
-                    {
-                        "type": "External",
-                        "name": participant["name"],
-                    }
-                )
-    return participants
+                return f"External as {participant['name']}"
 
 
-def get_use_case_steps(use_case: Any, use_case_steps: dict) -> list[dict]:
+def get_use_case_step(use_case: Any, use_case_step: dict) -> str:
     """
-    Helper method for extracting the participants from a use case definition.
+    Helper method for extracting the matching step information from a use case definition.
 
     Args:
-        use_case (Any): The use case definition from which to extract participants.
-        use_case_steps (dict): The dictionary of steps within the use case definition.
+        use_case (Any): The use case definition from which to extract the steps.
+        use_case_step (dict): The dictionary of a use case step to match against.
     Returns:
-        The list of participants and their data within the use case definition.
+        The use case step model information.
     """
     sequences: list[dict] = []
 
     # process steps
     steps = use_case["steps"]
     for step in steps:  # each step of a step type
-        if step in use_case_steps.keys():
-            use_case_step = use_case_steps[step].structure["usecase_step"]
+        if step in use_case_step.keys():
+            use_case_step = use_case_step.structure["usecase_step"]
         sequences.append(
             {
                 "name": use_case_step["name"],
