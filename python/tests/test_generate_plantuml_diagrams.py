b42e205699cc4c7fd77db162d7db008727125aa4
Lizz Condrey <ashley.condrey@ngc.com>
2024-04-17 15:35:24 +0000

    - corrected file content testing and split it out to a separate test

--- python/tests/test_generate_plantuml_diagrams.py
+++ python/tests/test_generate_plantuml_diagrams.py
@@ -69,14 +69,25 @@ class TestGeneratePlantUMLDiagrams(TestCase):
             self.assertIn("All AaC constraint checks were successful.", output_message)  # asserts the check command ran successful
             self.assertIn(temp_dir, output_message)  # asserts the puml-sequence command ran successfully
 
+    def test_cli_puml_sequence_file_output(self):
+        """Test the puml-sequence CLI command file output for the PUML Plugin."""
+        with TemporaryDirectory() as temp_dir:
+            aac_file_path = path.join(path.dirname(__file__), "alarm_clock/usecase.yaml")
+
+            args = [aac_file_path, temp_dir, "UNCLASSIFIED"]
+
+            exit_code, output_message = self.run_puml_sequence_cli_command_with_args(args)
+
             # Make sure files were created correctly
             temp_dir_files = listdir(temp_dir)
             self.assertEqual(2, len(temp_dir_files))
-            # self.assertIn("_sequence_diagram.puml", temp_dir_files)
             for temp_file in temp_dir_files:
                 self.assertTrue(temp_file.find("_sequence_diagram.puml"))
-                temp_content = open(temp_file)
+                temp_file_content = open(path.join(temp_dir, temp_file), "r")
+                temp_content = temp_file_content.read()
                 self.assertIn("Sequence Diagram", temp_content)
+                self.assertIn("participant", temp_content)
+                temp_file_content.close()
 
     def test_cli_puml_sequence_failure(self):
         """Test the puml-sequence CLI command failure for the PUML Plugin."""
@@ -90,7 +101,7 @@ class TestGeneratePlantUMLDiagrams(TestCase):
             self.assertNotEqual(0, exit_code)  # asserts the command failed
             self.assertGreater(len(output_message), 0)  # asserts the command produced output
             self.assertIn("No applicable use case definitions to generate a sequence diagram", output_message)  # asserts the puml-sequence command run failed
-            
+
     def test_puml_object(self):
 
         # TODO: Write success and failure unit tests for puml_object
